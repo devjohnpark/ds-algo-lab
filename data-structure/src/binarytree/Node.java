adedb007133f92669a918bd853e2d4c42a3419b6
devjohnpark <devjohnpark@gmail.com>
2025-05-01 08:10:35 +0900

    [Feat] add checkBinarySearchTree in binarytree.Node

--- data-structure/src/binarytree/Node.java
+++ data-structure/src/binarytree/Node.java
@@ -57,17 +57,33 @@ public class Node {
         }
     }
 
+    public boolean checkBinarySearchTree(Node root) {
+        return checkBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean checkBinarySearchTree(Node root, int min, int max) {
+        if (root == null) { // reach to left or right == null, return true
+            return true;
+        }
+        if (root.data < min || root.data > max) {
+            return false;
+        }
+        return checkBinarySearchTree(root.left, min, root.data - 1) && checkBinarySearchTree(root.right, root.data + 1, max);
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         Node root = new Node(scanner.nextInt());
-        while(scanner.hasNextInt()) {
+        while(scanner.hasNextInt()) { // exit: ctrl + D (EOF - 입력 종료)
             int value = scanner.nextInt();
             root.insert(value);
         }
         scanner.close();
 
-
         root.printInorder();
+
+        System.out.println("Is Binary Search Tree? " + root.checkBinarySearchTree(root));
     }
 }
